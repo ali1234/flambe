6202a6dd3aea1d2265a5fbfe27e24fefbd85cf44
Alistair Buxton <a.j.buxton@gmail.com>
2021-07-31 02:13:21 +0100

    Version 54.1
    Added analog fan control.

--- flambe/flambe.ino
+++ flambe/flambe.ino
@@ -1,4 +1,5 @@
-// rev 52.0
+// rev 54.1
+
 #include <Servo.h>
 
 #include <PinChangeInterrupt.h>
@@ -18,11 +19,16 @@ char *input_names[] = {"Low", "Medium", "High", "NoSignal"};
 #define R1_PWM_OUT (9)
 #define R2_PWM_OUT (6)
 #define R3_PWM_OUT (5)
-#define R4_PWM_OUT (3)
+#define FAN_PWM_OUT (3)
 
 #define R_OFF (45)
 #define R_ON (135)
 #define R_MID (90) // not used outside of testing
+
+#define F_OFF (45)
+#define F_MIN (80)
+#define F_MAX (180)
+#define F_THRESH (1900) // minimum input level to enable fan
 
 
 // trigger time bands in microseconds
@@ -36,19 +42,19 @@ unsigned long trigger_bands[4] {500, 1300, 1700, 2500};
 #define BAUD (57600)         // debug baud rate
 
 
-Servo relay1, relay2, relay3, relay4;
+Servo relay1, relay2, relay3, fan;
 volatile Input v_input = NoSignal;
+volatile unsigned long v_pulse_len = 0;
 
 // temporary variables
 volatile unsigned long v_rise_time_micros = 0;  // time when input signal went high in microseconds
 volatile unsigned long v_rise_time_millis = 0;  // time when input signal went high in milliseconds
 volatile unsigned long v_repeat_count = 0;
 
-void set_relays(unsigned char a, unsigned char b, unsigned char c, unsigned char d) {
+void set_relays(unsigned char a, unsigned char b, unsigned char c) {
     relay1.write(a);
     relay2.write(b);
     relay3.write(c);
-    relay4.write(d);
 }
 
 
@@ -61,9 +67,10 @@ void setup() {
     relay1.attach(R1_PWM_OUT);
     relay2.attach(R2_PWM_OUT);
     relay3.attach(R3_PWM_OUT);
-    relay4.attach(R4_PWM_OUT);
+    fan.attach(FAN_PWM_OUT);
 
-    set_relays(R_OFF, R_OFF, R_OFF, R_OFF);
+    set_relays(R_OFF, R_OFF, R_OFF);
+    fan.write(F_OFF);
 
     pinMode(C_PWM_IN, INPUT_PULLUP);
     attachPCINT(digitalPinToPinChangeInterrupt(C_PWM_IN), rising, RISING);
@@ -89,6 +96,7 @@ void rising(void)
 void falling(void)
 { 
     unsigned long pulse_len = micros() - v_rise_time_micros; // calculate length of pulse
+    v_pulse_len = pulse_len;
     if (
         (pulse_len > (trigger_bands[v_input] - HYSTERESIS)) &&
         (pulse_len < (trigger_bands[v_input+1] + HYSTERESIS))
@@ -112,7 +120,7 @@ void falling(void)
 }
 
 
-void flambe(Input input) {
+void flambe(Input input, unsigned long pulse_len) {
     // Start in Error state so that we can't immediately go full on at power up.
     static State state = Error;
   
@@ -142,29 +150,43 @@ void flambe(Input input) {
     // Update relays for state:
     switch(state) {
         case Idle:
-            set_relays(R_ON, R_OFF, R_ON, R_OFF);
+            set_relays(R_ON, R_OFF, R_ON);
             break;
         case On:
-            set_relays(R_ON, R_ON, R_ON, R_ON);
+            set_relays(R_ON, R_ON, R_ON);
             break;
         default: // handles Off and Error
-            set_relays(R_OFF, R_OFF, R_OFF, R_OFF);
+            set_relays(R_OFF, R_OFF, R_OFF);
             break;
+    }
+
+    Serial.print(", Fan: ");
+
+    // Update fan for state.
+    // For the fan to be On we must at least be in On state
+    // (ie trigger High.)
+    if (state == On && pulse_len > F_THRESH) {
+        unsigned long scaled = F_MIN + ((F_MAX - F_MIN) * ((pulse_len - F_THRESH) * 1.0 / (trigger_bands[3] - F_THRESH)));
+        fan.write(scaled);
+        Serial.print(scaled);
+    } else {
+        fan.write(F_OFF);
+        Serial.print(F_OFF);
     }
 }
 
 
-void passthrough(Input input) {
+void passthrough(Input input, unsigned long pulse_len) {
     // Mirror input on the output:
     switch(input) {
         case Medium:
-            set_relays(R_MID, R_MID, R_MID, R_MID);
+            set_relays(R_MID, R_MID, R_MID);
             break;
         case High:
-            set_relays(R_ON, R_ON, R_ON, R_ON);
+            set_relays(R_ON, R_ON, R_ON);
             break;
         default: // handles Low and NoSignal
-            set_relays(R_OFF, R_OFF, R_OFF, R_OFF);
+            set_relays(R_OFF, R_OFF, R_OFF);
             break;            
     }
 }
@@ -172,6 +194,7 @@ void passthrough(Input input) {
 
 void loop() {
     Input input;
+    unsigned long pulse_len;
 
     cli();
     if ((millis() - v_rise_time_millis) > TIMEOUT) {
@@ -182,6 +205,7 @@ void loop() {
         }
     }
     input = v_input;
+    pulse_len = v_pulse_len;
     sei();
 
     Serial.print("Input: ");
@@ -189,8 +213,8 @@ void loop() {
     Serial.print(", Repeats: ");
     Serial.print(v_repeat_count);
     
-    //passthrough(input);
-    flambe(input);
+    //passthrough(input, pulse_len);
+    flambe(input, pulse_len);
 
     Serial.print("\n");
 
